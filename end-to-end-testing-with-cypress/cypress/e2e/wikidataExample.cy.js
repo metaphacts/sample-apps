1e92c697265b72e4085f123f60941f186502ec4c
Patrick Doyle <pd@metaphacts.com>
2025-07-10 02:14:57 -0400

    Adjust Wikidata IRI for Albert Einstein used in a Cypress Test

--- end-to-end-testing-with-cypress/cypress/e2e/wikidataExample.cy.js
+++ end-to-end-testing-with-cypress/cypress/e2e/wikidataExample.cy.js
@@ -7,7 +7,7 @@ describe('template spec', function () {
   it('Verify navigation to Albert Einstein (Entity Q937)', () => {
     cy.get('#react-select-2-input').type('einstein');
     cy.get('#react-select-2-option-0 > span').click();
-    cy.get('.knowledgeGraphBar__uriRow > a > :nth-child(1)').should('have.text', 'http://www.wikidata.org/entity/Q937');
+    cy.get('.knowledgeGraphBar__uriRow > a > :nth-child(1)').should('have.text', 'http://www.wikidata.org/entity/Q16834800');
   })
 
 })
